a953ccf7a84d6784919a7a35bbd1c0bccf03990e
Anton Hangano <anton.hangano@gmail.com>
2021-10-11 17:00:42 +0200

    feat:admin page creation and style moddified

---
+++ admin.php
@@ -0,0 +1,27 @@
+<?php
+	include 'includes/header.html';
+	include 'includes/nav.php';  
+?>
+
+    <main class="container pt-2">
+        <h1>Dashboard - Main Menu</h1>
+
+        <div class="list-group">
+        <a href="adminBrands.php" class="list-group-item list-group-item-action">
+        Brands Administration Panel
+        </a>
+        <a href="adminCategories.php" class="list-group-item list-group-item-action">
+        Categories Administration Panel
+        </a>
+        <a href="adminProducts.php" class="list-group-item list-group-item-action">
+        Products Administration Panel
+        </a>
+        <a href="adminUsers.php" class="list-group-item list-group-item-action">
+        Users Administration Panel
+        </a>
+
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>
